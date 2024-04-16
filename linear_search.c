28646f4298069084882a8cb1a762986687e0f5c5
Aruljayaraj-AFX <aruljayarajj826@gmail.com>
2024-04-17 01:41:48 +0530

    make as perfect

--- linear_search.c
+++ linear_search.c
@@ -1,31 +1,31 @@
 #include<stdio.h>
-void main()
+int main()
 {
     int a;
     printf("Enter the how many in the list:");
     scanf("%d",&a);
 
-    char b[a][1000];
-    char c[1000];
+    int b[a];
+    int c;
 
     for ( int i = 0; i < a; i++)
     {
         printf("Enter the %d element", i + 1 );
-        scanf(" %[^\n]%*c", &b[i]);
+        scanf("%d", &b[i]);
     }
     
     printf("Enter the search element :");
-    scanf(" %[^\n]%*c", &c);
+    scanf("%d", &c);
     for (int j = 0; j < a; j++){
-        if (strcmp(b[j], c) == 0)
+        if (b[j] == c)
         {
             printf("Search element in the %d position",  j + 1 );
-            break;
+            return 0;
         }
         else if( j == (  a - 1 ) )
         {
             printf("Not present in the list");
         }
-    }
+    }return 0;
 }
 
