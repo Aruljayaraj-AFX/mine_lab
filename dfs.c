c98a13a5324397dbf8edf1a1ff37dac7251226be
Aruljayaraj-AFX <aruljayarajj826@gmail.com>
2024-04-19 13:11:24 +0530

    successfully finish

--- dfs.c
+++ dfs.c
@@ -1,65 +1,41 @@
 #include <stdio.h>
-#include <string.h>
 
-void dd(int s, char a[], char connected_nodes[][s*5], char ss[], int y, int t);
-int indexx(char h, char a[], int s);
+int visited[10]; 
+
+void dfs(int v, int a, char c[a], int o[a][a]) {
+    printf("%c ", c[v]);
+    visited[v] = 1;
+    for (int i = 0; i < a; ++i) {
+        if ((o[v][i]==1) && (visited[i] != 1)) {
+            dfs(i, a, c, o);
+        }
+    }
+}
 
 int main() {
-    int s, d;
-    printf("How many nodes or vertices present in the graph or tree: ");
-    scanf("%d", &s);
-    char a[s];
-    char connected_nodes[s][s*5];
+    int a;
+    printf("How many nodes are present in the graph or tree: ");
+    scanf("%d", &a);
+    char c[a];
+    for (int i = 0; i < a; i++) {
+        printf("Enter the node %d: ", i + 1);
+        scanf(" %c", &c[i]);  
+    }
+    int o[a][a];
 
-    for (int i = 0; i < s; i++) {
-        printf("Enter the %d node: ", i + 1);
-        scanf(" %c", &a[i]);
-
-        printf("How many nodes are connected to %c: ", a[i]);
-        scanf("%d", &d);
-
-        printf("Enter the connected nodes orderly without spaces: ");
-        for (int j = 0; j < d; j++) {
-            scanf(" %c", &connected_nodes[i][j]);
+    for (int u = 0; u < a; u++) {
+        for (int uu = 0; uu < a; uu++) {
+            printf("Enter 1 if %c and %c are connected, otherwise 0: ", c[u], c[uu]);
+            scanf("%d", &o[u][uu]);
         }
     }
-    char ss[s];
-    int y = 0;
-    int t = 0;
-    dd(s, a, connected_nodes, ss, y, t);
+
+    printf("\nDepth First Search Path:\n");
+    for (int i = 0; i < a; ++i) {
+        visited[i] = 0;
+    }
+    dfs(0, a, c, o); 
+    printf("\n");
 
     return 0;
-}
-
-
-int indexx(char h, char a[], int s) {
-    for (int i = 0; i < s; i++) {
-        if (a[i] == h) {
-            return i;
-        }
-    }
-    // Return -1 if h is not found in a[]
-    return -1;
-}
-
-void dd(int s, char a[], char connected_nodes[][s*5], char ss[], int y, int t) {
-    int g = 0;
-    for (int u = 0; u < s; u++) {
-        /*printf("%c\n", a[u]);*/
-        for (int i = 0; i < t; i++) {
-            if (a[u] == ss[i]) {
-                g = 1;
-            }
-        }
-        if (g == 0) {
-            ss[t] = a[u]; // Remember to append the t value
-            y = indexx(a[u], a, s);
-            int p = strlen(connected_nodes[y]);
-            char o[p];
-            strcpy(o, connected_nodes[y]);
-            t++;
-            dd(p, o, connected_nodes, ss, y, t);
-        }
-
-    }
 }
